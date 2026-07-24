c27fe0917c26128ec5a0d77ed52d47e222fbfb69
fanjunjie <fanjunjie@smartplg.com>
2026-07-24 16:21:33 +0800

    修正 jwt token 问题

--- btx-framework/btx-framework-boot-security-shiro/src/main/java/top/cheesetree/btx/framework/security/shiro/support/jwt/BtxSecurityJwtAuthorizingRealm.java
+++ btx-framework/btx-framework-boot-security-shiro/src/main/java/top/cheesetree/btx/framework/security/shiro/support/jwt/BtxSecurityJwtAuthorizingRealm.java
@@ -170,6 +170,7 @@ public class BtxSecurityJwtAuthorizingRealm extends AuthorizingRealm {
                 jwt.setIssuedAt(new Date());
                 t.setAccessToken(jwt.sign());
 
+                ((JwtToken) token).setJwt(t.getAccessToken());
                 u.setAuthinfo(t);
                 return new SimpleAuthenticationInfo(new SimplePrincipalCollection(u, "user"), t.getAccessToken());
             } else {
@@ -182,6 +183,6 @@ public class BtxSecurityJwtAuthorizingRealm extends AuthorizingRealm {
 
     @Override
     protected Object getAuthenticationCacheKey(AuthenticationToken token) {
-        return super.getAuthenticationCacheKey(token);
+        return token != null ? ((JwtToken) token).getJwt() : null;
     }
 }
