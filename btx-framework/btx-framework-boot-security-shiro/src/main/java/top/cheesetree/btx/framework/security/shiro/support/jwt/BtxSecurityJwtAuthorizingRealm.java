506748de979353caaa8161c78ad26ee18f5fb303
fanjunjie <fanjunjie@smartplg.com>
2026-07-24 14:26:33 +0800

    修正 jwt token 问题

--- btx-framework/btx-framework-boot-security-shiro/src/main/java/top/cheesetree/btx/framework/security/shiro/support/jwt/BtxSecurityJwtAuthorizingRealm.java
+++ btx-framework/btx-framework-boot-security-shiro/src/main/java/top/cheesetree/btx/framework/security/shiro/support/jwt/BtxSecurityJwtAuthorizingRealm.java
@@ -182,6 +182,6 @@ public class BtxSecurityJwtAuthorizingRealm extends AuthorizingRealm {
 
     @Override
     protected Object getAuthenticationCacheKey(AuthenticationToken token) {
-        return super.getAuthenticationCacheKey(token);
+        return token != null ? ((JwtToken) token).getJwt() : null;
     }
 }
